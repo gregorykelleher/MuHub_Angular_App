0b9623a08359ff0d345e6561d39d84682fd4ea5b
aidandonnelly96 <aidandonnelly96@hotmail.com>
2016-11-01 17:27:19 +0000

    Revert "fixing sync errors":
    This reverts commit 7576832d9d4f88c70eb05feca75eb803165d6d2d.

--- public/app/js/aiml.js
+++
@@ -1,6 +0,0 @@
-AIMLInterpreter = require('./AIMLInterpreter');
-var aimlInterpreter = new AIMLInterpreter({name:'WireInterpreter', age:'42'});
-aimlInterpreter.loadAIMLFilesIntoArray(['./test.aiml.xml']);
-var callback = function(answer, wildCardArray, input){
-console.log(answer + ' | ' + wildCardArray + ' | ' + input);
-};
