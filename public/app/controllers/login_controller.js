88a5c82b9956c443da7a91be57e703c7440036e0
gregory kelleher <gregory@gregorykelleher.com>
2016-11-03 20:51:17 +0000

    added basic register and sign in functionality

--- public/app/controllers/login_controller.js
+++ public/app/controllers/login_controller.js
@@ -5,11 +5,37 @@
 
 	function login_controller() {
 		var self = this;
-		self.auth = auth;
+		self.login = login;
 
-		function auth($scope) {
+		function login($scope, $firebaseAuth, Auth) {
+
+			var auth = $firebaseAuth();
+
+			$scope.register = function() {
+				$scope.message = null;
+				$scope.error = null;
+
+				Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+				.then(function(firebaseUser) {
+					$scope.message = "User created with uid: " + firebaseUser.uid;
+				}).catch(function(error) {
+					$scope.error = error;
+				});
+			}
+
+			$scope.sign_in = function() {
+				$scope.message = null;
+				$scope.error = null;
+
+				Auth.$signInWithEmailAndPassword($scope.email, $scope.password)
+				.then(function(firebaseUser) {
+					$scope.message = "User signed in with uid: " + firebaseUser.uid;
+				}).catch(function(error) {
+					$scope.error = error;
+				});
+			}
 
 		};
 	}
-
+	
 })();
--- public/app/js/firebase.js
+++ public/app/js/firebase.js
@@ -12,3 +12,4 @@ var database = firebase.database();
 
 
 
+
