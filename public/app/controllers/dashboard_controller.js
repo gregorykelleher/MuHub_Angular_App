d7715e5e1b03d524fe1187bdb999fa2533653faf
gregory kelleher <gregory@gregorykelleher.com>
2016-11-28 23:59:20 +0000

    changed ctrl names and made weather template

---
+++ public/app/controllers/dashboard_controller.js
@@ -0,0 +1,48 @@
+(function(){
+	angular
+	.module('main')
+	.controller('dashboard_controller', [dashboard_controller]);
+
+	function dashboard_controller() {
+		var self = this;
+		self.weather = weather;
+		self.map = map;
+
+		function weather($scope) {
+
+			$scope.day_1 = "Monday";
+			$scope.temp_1 = "18ºC";
+
+			$scope.day_2 = "Tuesday";
+			$scope.temp_2 = "10ºC";
+
+			$scope.day_3 = "Wednesday";
+			$scope.temp_3 = "9ºC";
+
+		};
+
+		function map($scope, NgMap, $firebaseArray, Data) {
+
+			// add map to scope
+			NgMap.getMap().then(function(map) {
+				$scope.map = map;
+			});
+
+			// pull location data from firebase
+			$scope.locations = $firebaseArray(Data.child('locations'));
+
+			$scope.addMarker = function(item) {
+				
+				// display marker and info window
+				$scope.location = item;
+				$scope.map.showInfoWindow('info', 'marker');
+
+				// dynamic map re-centering
+				var coords = new google.maps.LatLng(item.coords.lat, item.coords.lng);
+				$scope.map.panTo(coords);
+
+			}
+		};
+	}
+	
+})();
