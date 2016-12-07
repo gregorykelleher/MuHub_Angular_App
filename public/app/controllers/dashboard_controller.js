1c65a7fd2b62456f16354c2b2f8f56e94be4ece8
gregory kelleher <gregory@gregorykelleher.com>
2016-12-07 14:58:25 +0000

    partially working query

--- public/app/controllers/dashboard_controller.js
+++ public/app/controllers/dashboard_controller.js
@@ -8,6 +8,7 @@
 		self.weather = weather;
 		self.map = map;
 		self.chat = chat;
+		self.timetable;
 
 		function weather($scope) {
 
@@ -16,8 +17,7 @@
 			$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Maynooth,ie&units=metric&APPID=b7c4cdf421b48042847185d7ace4855a").then(function(wd) {
 				$scope.location = (wd.city.name).toString();
 				$scope.weather = (wd.list[0].weather[0].main).toString();
-				$scope.temp = (wd.list[0].main.temp).toString() + "°C";
-
+				$scope.temp = (wd.list[0].main.temp).toString().split('.')[0] + "°C";
 
 				switch ($scope.weather) {
 					case "Rain":
@@ -69,8 +69,7 @@
 
 		function chat($scope, $firebaseArray, $firebaseObject, Data, Auth, $timeout) {
 
-			var uid = Auth.$getAuth().uid;
-			$scope.uid = Auth.$getAuth().uid;
+			$scope.current_user_id = Auth.$getAuth().uid;
 
 			$scope.rooms = $firebaseArray(Data.child('users'));
 			$scope.users = [];
@@ -101,6 +100,112 @@
 
 				$scope.receiver = item.first_name;
 				$scope.receiver_id = item.id;
+
+				$scope.rooms = $firebaseArray(Data.child("rooms"));
+
+				// console.log("Gregory: " + $scope.current_user_id);
+				// console.log("Receiver: " + $scope.receiver_id);
+
+				function createRoom(current_user_id, receiver_id) {
+
+					var room_key = Data.child('rooms').push().key;
+
+
+					$scope.rooms.$add({
+						room_key: room_key,
+						usr_1: current_user_id,
+						usr_2: receiver_id
+					});
+
+					Data.child('rooms').on("child_added", function(snapshot) {
+						var data = snapshot.val();
+						console.log("room_key: " + data.room_key);
+						console.log("usr_1: " + data.usr_1);
+						console.log("usr_2: " + data.usr_2);
+					});
+
+				}
+
+				// createRoom($scope.current_user_id, $scope.receiver_id);
+
+				Data.child('rooms').orderByChild('usr_2').equalTo($scope.receiver_id).once('value', function(snapshot){
+					snapshot.forEach(function(userSnapshot) {
+						$scope.data = userSnapshot.val();
+						if ($scope.data.usr_1 == $scope.current_user_id) {
+							console.log("room already exists with current user and receiver user");
+						}
+					});
+				});
+
+				 // var query = Data.child("rooms").orderByChild("member_1").limitToLast(10);
+				 // var list = $firebaseArray(query);
+				 // console.log(list);
+
+
+				// if (roomCheck($scope.current_user_id, $scope.receiver_id) == true) {
+				// 	createRoom($scope.current_user_id, $scope.receiver_id);
+				// }
+
+				// Data.child("users").orderByChild("id").equalTo("sh09YVUPM6PDRszQyvkO6Fjzc6x2").once("value", function(snapshot) {
+				// 	var userData = snapshot.val();
+				// 	if (userData){
+				// 		console.log("exists!");
+				// 	}
+				// });
+
+
+				// Data.child("rooms").orderByChild("member_1").equalTo($scope.current_user_id).once("value", function(snapshot) {
+				// 	var userData = snapshot.val();
+				// 	if (userData){
+				// 		console.log("exists!");
+				// 	}
+				// });
+
+				// Data.child("rooms").once("value", function(snapshot) {
+				// 	snapshot.forEach(function(childSnapshot) {
+				// 		var key = childSnapshot.key();
+				// 		console.log(key);
+				// 		var childData = childSnapshot.val();
+				// 	})
+				// });
+
+
+				// Data.child("rooms").orderByChild("member_1").equalTo($scope.current_user_id).once("value", function(snapshot) {
+				// 	console.log(snapshot.val());
+				// }, function (err) {
+				// 	console.log("can't");
+				// });
+
+				// Data.child("rooms").equalTo($scope.receiver).once("value", function(snapshot) {
+				// 	snapshot.forEach(function(data) {
+				// 		console.log(data.val());
+				// 	});
+				// });
+				
+				// Data.child("rooms").once("value", function(snapshot) {
+				// 	snapshot.forEach(function(childSnapshot) {
+				// 		var childData = childSnapshot.val();
+				// 		console.log(childData);
+				// 	});
+				// });
+
+
+
+				// function roomCheck(current_user_id) {
+
+				// 	console.log($firebaseArray(Data.child('users')));
+
+				// 	Data.child('users').orderByChild("id").equalTo("current_user_id").once("value", function(snapshot) {
+				// 		var userData = snapshot.val();
+				// 		console.log(userData);
+				// 		if (userData){
+				// 			console.log("exists!");
+				// 		}
+				// 		else console.log("nope!");
+				// 	});
+				// }
+				// roomCheck($scope.current_user_id);
+
 
 				// dynamic user chat tab
 				if ($scope.tabs.length == 1) {
