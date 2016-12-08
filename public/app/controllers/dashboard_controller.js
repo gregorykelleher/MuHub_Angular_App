4e49bbb39679c7452deb9f0b3246fb60afdb2ebf
Tomek Gawrysiak <tommygaw95@gmail.com>
2016-12-08 19:15:38 +0000

    changed the weather api and now it supports https.
    fixed location error

--- public/app/controllers/dashboard_controller.js
+++ public/app/controllers/dashboard_controller.js
@@ -12,27 +12,57 @@
 
 			$scope.day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][(new Date()).getDay()];
 
-			$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Maynooth,ie&units=metric&APPID=22d446acf6fed1e84d9fa2f7eb4a89ac").then(function(wd) {
-				$scope.location = (wd.city.name).toString();
-				$scope.weather = (wd.list[0].weather[0].main).toString();
-				$scope.temp = (wd.list[0].main.temp).toString() + "°C";
+			$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%20561669%20and%20u%3D%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").then(function(wd) {
+				$scope.location = "Maynooth";
+				$scope.weather = (wd.query.results.channel.item.condition.code).toString();
+				$scope.temp = (wd.query.results.channel.item.condition.temp).toString().split('.')[0] + "°C";
 
 				switch ($scope.weather) {
-					case "Rain":
+					case "10":
+					case "11":
+					case "12":
 					$scope.img = "/app/imgs/rain.svg";
 					break;
-					case "Drizzle":
+					case "8":
+					case "9":
 					$scope.img = "/app/imgs/rain.svg";
 					break;
-					case "Clouds":
+					case "26":
+					case "27":
+					case "28":
+					case "29":
+					case "30":
+					case "20":
 					$scope.img = "/app/imgs/clouds.svg";
-					case "Clear":
+					break;
+					case "31":
+					case "32":
+					case "33":
+					case "34":
+					case "36":
+					case "27":
 					$scope.img = "/app/imgs/clear.svg";
 					break;
-					case "Snow":
+					case "5":
+					case "6":
+					case "7":
+					case "13":
+					case "14":
+					case "15":
+					case "16":
+					case "17":
+					case "41":
+					case "42":
+					case "43":
+					case "46":
 					$scope.img = "/app/imgs/snow.svg";
 					break;
-					case "Thunderstorm":
+					case "3":
+					case "4":
+					case "37":
+					case "38":
+					case "39":
+					case "40":
 					$scope.img = "/app/imgs/thunderstorm.svg";
 					break;
 					default:
